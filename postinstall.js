b6d7b227b67f0821b56763aec759a70e2438b305
i5ting <i5ting@126.com>
2016-04-07 09:50:48 +0800

    refact

--- postinstall.js
+++ postinstall.js
@@ -2,19 +2,30 @@
 
 // 在npm postinstall之后，创建config/mongodb.example.js（如果不存在的话）,提示用户修改“cp config/mongodb.example.js config/mongodb.js”
 
-const _path = process.cwd() + '/config/mongodb.js'
-const _examle_file_path = process.cwd() + "/config/mongodb.example.js"
+
+var project_path = process.cwd().replace('\/node_modules\/mongooseconfig', '') + "/config"
+const _path =  project_path + '/mongodb.js'
+const _examle_file_path = project_path + "/mongodb.example.js"
 
 var fs = require('fs');
 
 var is_exist = fs.existsSync(_path);
-
+console.log(project_path)
+console.log(_path)
 console.log(is_exist);
 
-if (is_exist === true) {
-  console.log("[MONGOOSE CONFIG] INFO " + _path + ' is exist')
-} else {
-  console.log("[MONGOOSE CONFIG] INFO " + _path + ' is not exist, now create a example file')
+var mkdirp = require('mkdirp');
+    
+mkdirp(project_path, function (err) {
+    if (err) console.error(err)
+    else console.log('("[MONGOOSE CONFIG] INFO create config folder')
+      
+    if (is_exist === true) {
+      console.log("[MONGOOSE CONFIG] INFO " + _path + ' is exist')
+    } else {
+      console.log("[MONGOOSE CONFIG] INFO " + _path + ' is not exist, now create a example file')
   
-  fs.createReadStream(__dirname + "/config/mongodb.example.js").pipe(fs.createWriteStream( _examle_file_path ))
-}
+      fs.createReadStream(__dirname + "/config/mongodb.example.js").pipe(fs.createWriteStream( _examle_file_path ))
+    }
+});
+
